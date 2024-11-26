6d995accd1fbc45394f9ecf0504f26080863f1cc
DawKun <kunickidawid@wp.pl>
2024-11-26 16:11:54 +0100

    prescription preview

---
+++ MediMax/Main/PodgladReceptWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows;
+
+namespace Main
+{
+    public partial class PodgladReceptWindow : Window
+    {
+        public PodgladReceptWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            string pesel = PeselTextBox.Text;
+            if (!IsPeselValid(pesel))
+            {
+                MessageBox.Show("Wprowadź prawidłowy numer PESEL.");
+                return;
+            }
+
+            LoadRecepty(pesel);
+        }
+
+        private void LoadRecepty(string pesel)
+        {
+            using (var context = new MediMaxEntities())
+            {
+                var recepty = from r in context.tbl_Recepta
+                              join l in context.tbl_Leki on r.IdLeku equals l.Id
+                              where r.PESEL == pesel
+                              select new
+                              {
+                                  NumerRecepty = r.NumerRecepty,
+                                  LekNazwa = l.Nazwa,
+                                  CzyZrealizowano = r.CzyZrealizowano ? "Zrealizowana" : "Nie zrealizowana"
+                              };
+
+                var receptyList = recepty.ToList();
+
+                if (!receptyList.Any())
+                {
+                    MessageBox.Show("Brak recept dla podanego PESELu.", "Informacja", MessageBoxButton.OK, MessageBoxImage.Information);
+                    ReceptyDataGrid.ItemsSource = null;
+                }
+                else
+                {
+                    ReceptyDataGrid.ItemsSource = receptyList;
+                }
+            }
+        }
+
+
+        private void PeselTextBox_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextNumeric(e.Text);
+        }
+
+        private static bool IsTextNumeric(string text)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            return !regex.IsMatch(text);
+        }
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+
+        private bool IsPeselValid(string pesel)
+        {
+            if (pesel.Length != 11 || !long.TryParse(pesel, out _)) return false;
+
+            int[] weights = { 9, 7, 3, 1, 9, 7, 3, 1, 9, 7 };
+            int sum = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                sum += (pesel[i] - '0') * weights[i];
+            }
+
+            int checksum = sum % 10;
+            return checksum == (pesel[10] - '0');
+        }
+    }
+}
